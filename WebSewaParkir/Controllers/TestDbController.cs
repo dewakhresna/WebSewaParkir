8fa19aa97e525f2478bdf97f305b01c5ce220922
dewa khresna <dewakhresna04@gmail.com>
2025-10-15 08:33:19 +0700

    Add Connection With WebConfig

---
+++ WebSewaParkir/Controllers/TestDbController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSewaParkir.Controllers
+{
+    public class TestDbController : Controller
+    {
+        // GET: TestDb
+        public ActionResult Index()
+        {
+            // Ambil koneksi dari Web.config
+            string connectionString = System.Configuration.ConfigurationManager
+                .ConnectionStrings["MyDbConnection"].ConnectionString;
+
+            string message = "";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    message = "✅ Koneksi ke database SewaParkir BERHASIL!";
+                }
+                catch (Exception ex)
+                {
+                    message = "❌ Gagal koneksi ke database: " + ex.Message;
+                }
+            }
+
+            return Content(message);
+        }
+    }
+}
---
+++ WebSewaParkir/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebSewaParkir.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(70)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string Email { get; set; }
+
+        [StringLength(15)]
+        public string Telp { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+    }
+}
